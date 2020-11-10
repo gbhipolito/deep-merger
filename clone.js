3a7db5d44e7ffb88048ca0d409b113ca7b554bbb
glyphdevs <glyphdevs@gmail.com>
2020-11-10 22:42:21 +0800

    do not merge arrays, just replace

--- clone.js
+++ clone.js
@@ -1,11 +1,17 @@
 const clone = (toObj, fromObj) => {
   if (fromObj.constructor === Array) {
-    // if they're array, just push values of fromObj to toObj
-    for (let fromIdx = 0; fromIdx < fromObj.length; fromIdx++) {
-      if (!toObj.includes(fromObj[fromIdx])) {
-        toObj.push(fromObj[fromIdx]);
-      }
-    }
+    /* if they're array, just push values of fromObj to toObj */
+    // for (let fromIdx = 0; fromIdx < fromObj.length; fromIdx++) {
+    //   if (!toObj.includes(fromObj[fromIdx])) {
+    //     toObj.push(fromObj[fromIdx]);
+    //   }
+    // }
+
+    /* replace array, don't merge */
+    console.log('replace', toObj, 'with', fromObj)
+    toObj.length = 0;
+    toObj.push(...fromObj);
+    
   } else if (fromObj.constructor === Object) {
     // if they're object, deep clone via recursion
 
--- index.js
+++ index.js
@@ -5,3 +5,33 @@ module.exports = {
     immutateDeepMerge,
     mutateDeepMerge,
 }
+
+const testA = {
+    a: 1,
+    b: '2',
+    c: {
+        d: 'asdf',
+        e: null,
+        f: [1, 2, 5]
+    },
+    g: ['a','b','d'],
+    h: {
+        i: 1,
+    }
+};
+
+const testB = {
+    b: 5,
+    c: {
+        j: '123',
+        d: null
+    },
+    h: 7
+};
+
+const testC = {
+    g: [1,2,3]
+};
+
+console.log({testA, testB, testC});
+console.log(immutateDeepMerge(testA, testB, testC));
